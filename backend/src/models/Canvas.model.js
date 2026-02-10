dd38eddba9b64ae0e21f78317123d00f93d40f84
pattatejeswar <pattatejeswar@gmail.com>
2026-02-10 18:07:01 +0530

    added code for canvas model

---
+++ backend/src/models/Canvas.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const canvasSchema = new mongoose.Schema(
+    {
+        ownerId: {
+            // Modified to Mixed to allow 'single-user' string for prototype AND ObjectId for auth
+            type: mongoose.Schema.Types.Mixed,
+            required: true,
+            default: 'single-user',
+            ref: 'User',
+        },
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+            default: 'Untitled Canvas',
+        },
+    },
+    {
+        timestamps: true, // auto createdAt, updatedAt
+    }
+);
+
+export default mongoose.model('Canvas', canvasSchema);
